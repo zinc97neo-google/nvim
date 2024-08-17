359a63d6a720e545e84db0aa55ddd61b3540e1d5
zinc <zinc97neo@gmail.com>
2024-08-17 22:37:36 +0800

    feat(global): add git plugin

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -5,10 +5,26 @@ local toggleterm = {
   config = function()
     require("toggleterm").setup({
       direction = "float",
+      shell = "nu"
     })
   end
 }
 
+local gitsigns = {
+  'lewis6991/gitsigns.nvim',
+  lazy = false,
+  config = function()
+    require('gitsigns').setup()
+  end,
+}
+
+local git_messenger = {
+  'rhysd/git-messenger.vim',
+  lazy = false,
+}
+
 editor = {
-  toggleterm
+  toggleterm,
+  gitsigns,
+  git_messenger
 }
